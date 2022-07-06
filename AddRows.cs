4ce40bb0adcfe401b971464ce318ce34d6f7b9e0
Caprese17 <84934222+Caprese17@users.noreply.github.com>
2022-07-06 15:59:07 -0400

    Add files via upload

---
+++ AddRows.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AddRows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2d = { { 5, 8, 4 }, { 1, 9, 3 }, { 11, 2, 6 } };
+            Console.WriteLine("The Orginal Array Values are: ");
+            for (int r = 0; r < array2d.GetLength(0); r++)
+            {
+                for (int c =0; c < array2d.GetLength(1); c++)
+                {
+                    Console.Write(array2d[r, c] + "\t");
+                }
+                Console.WriteLine();
+            }
+            //this section will add the rows and put the sum into position 0 of the row
+            int sum = 0;
+            
+            for (int r = 0; r < array2d.GetLength(0); r++)
+            {
+                for (int c = 0; c < array2d.GetLength(1); c++)
+                {
+                    sum = array2d[r, c] + sum; // sum+= arrray2d[r,c];
+
+                }
+                array2d[r, 0] = sum;
+                sum = 0;
+            }
+
+            Console.WriteLine("The rows are added and total  put into first position: ");
+            for (int r = 0; r < array2d.GetLength(0); r++)
+            {
+                for (int c = 0; c < array2d.GetLength(1); c++)
+                {
+                    Console.Write(array2d[r, c] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ArrayPractice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace In_class_Array_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum = 0;
+            int col = 0;
+
+            int[,] array3DA = { { 5, 8, 4 }, { 1, 9, 3 }, { 11, 2, 6 } };
+
+            for (int r = 0; r < array3DA.GetLength(0); r++)
+            {
+                
+                for (int c = 0; c < array3DA.GetLength(1); c++)
+                {
+                    sum =+ array3DA[r, c]; // sum = sum + array3DA[r,c];
+                    Console.Write(array3DA[r, c] + "\t");
+                    //Console.WriteLine(sum);
+                }
+                
+                array3DA[r, col] = sum;
+                Console.WriteLine(array3DA[r, col] + "\t");
+               //Console.WriteLine(sum);
+            }
+            
+            
+        }
+    }
+}
